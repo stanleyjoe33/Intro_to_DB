d8189c46e43911743cd0563887ec447fe52f142f
Joseph Stanley <stanleyojoseph33@gmail.com>
2025-08-17 22:54:08 +0100

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- Task 4: Print full description of the table books
+
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
